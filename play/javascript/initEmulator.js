0e709a47224c8d5b87b7a566f16150d39627b393
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2022-11-13 21:05:36 +0100

    refactor: move emulator init script to file

---
+++ play/javascript/initEmulator.js
@@ -0,0 +1,24 @@
+import init from "../assets/wasm/emulator/chip8_emu.js";
+
+// Attach start function to the starting element
+document
+  .getElementById("emulatorStarter")
+  .addEventListener("click", startEmulator);
+
+function startEmulator() {
+  // Hide starting element to prevent re-initialization
+  document.getElementById("emulatorStarter").classList.add("hide");
+
+  // Run the webassembly
+  init().then(() => {
+    console.log("Loaded WASM");
+
+    // Scale the canvas to its parent
+    let parent = document.getElementById("wasm-chip8-canvas");
+    parent.lastElementChild.style.width = "100%";
+    parent.lastElementChild.style.height = "100%";
+
+    // Focus on the canvas
+    parent.lastElementChild.focus();
+  });
+}
